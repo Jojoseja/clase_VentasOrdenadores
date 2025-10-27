02c95444d0b0b3230b363f56186fd4f85a24beb3
jojoseja <jfeyjoo@gmail.com>
2025-10-27 12:34:08 +0100

    first commit

---
+++ src/main/java/com/mycompany/clase_ventasordenadores/Ventas.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.clase_ventasordenadores;
+
+/**
+ *
+ * @author jojo
+ */
+public class Ventas {
+    
+}
